56318dbc6d087d3e9f40f0a61e577f2f6e90da97
BhanuPratap-Singh-Naruka <pratapbhanu0187@gmail.com>
2023-06-22 12:57:05 +0530

    feat: configured storybook

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,11 @@
-export default function Home() {
-  return <main>Home Page</main>;
+import Button from "../stories/Button";
+
+function Home() {
+	return (
+		<div>
+			<h1>Home Page</h1>
+		</div>
+	);
 }
+
+export default Home;
